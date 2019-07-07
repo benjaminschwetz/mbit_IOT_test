0c93f0b6f7ca6dc831c224af811d7ca15cf2d1b1
Benjamin Schwetz <benjamin.schwetz@gmx.de>
2019-07-07 12:07:20 +0200

    init

--- main.ts
+++ main.ts
@@ -1,7 +1,2 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+ESP8266_IoT.initwifi(SerialPin.P8, SerialPin.P12, BaudRate.BaudRate115200)
+ESP8266_IoT.connectwifi("your ssid", "your key")
